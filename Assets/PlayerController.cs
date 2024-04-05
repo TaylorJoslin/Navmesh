102abb5aaf88b82e181c30acb60066030f4a8130
tjjoslin <124922955+tjjoslin@users.noreply.github.com>
2024-04-05 12:06:01 -0400

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Debug.Log("ray.origin" + ray.origin + "ray.direction " + ray.direction);
+            Debug.DrawRay(ray.origin, ray.direction, Color.red);
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.point);
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
