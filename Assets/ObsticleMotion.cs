8d54aad992d5b1e3df92eeffde25687943a537f1
TaylorJoslin <143643796+TaylorJoslin@users.noreply.github.com>
2024-04-12 13:17:34 -0400

    update

--- Assets/ObsticleMotion.cs
+++ Assets/ObsticleMotion.cs
@@ -1,19 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class ObsticleMotion : MonoBehaviour
+public class RepeatingFollowPath : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
+    [SerializeField] private float speed;
+    [SerializeField] private Transform[] points;
+    private NavMeshAgent agent;
+    private int pointIndex;
+
     void Start()
     {
-        
+        pointIndex = 0;
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(points[pointIndex].position);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        agent.speed = Random.Range(3,10);
+
+        // Check if AI has reached current waypoint
+        if (!agent.pathPending && agent.remainingDistance < 0.1f)
+        {
+            // Move to next waypoint
+            pointIndex = (pointIndex + 1) % points.Length;
+            agent.SetDestination(points[pointIndex].position);
+        }
     }
 }
--- Assets/myAgentPath.cs
+++ Assets/myAgentPath.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class myAgentPath : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //[SerializeField] Vector3 mouse;
+    [SerializeField] Transform mouse;
+
+    
+    NavMeshAgent myAgent;
     void Start()
     {
-        
+       
+        myAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos = mouse.transform.position;
+
+        myAgent.speed = Random.Range(3, 10);
+
+        myAgent.SetDestination(mousePos);
     }
 }
